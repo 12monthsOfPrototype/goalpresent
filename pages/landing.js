15da645da3c5acd27ee4c13b62abcaef3f68878e
Richard <richardmenning@googlemail.com>
2020-04-22 23:44:00 +0200

    started building lp

---
+++ pages/landing.js
@@ -0,0 +1,18 @@
+import Layout from '../components/Layout';
+
+const landing = () => {
+  return (
+    <Layout>
+      <div className="container-fluid">
+        <h1>
+          <span>Goalpresent</span> - The easy way to achieve your goals
+        </h1>
+
+        <button>Leanr more</button>
+        <button>Sign up</button>
+      </div>
+    </Layout>
+  );
+};
+
+export default landing;
