926dd1549cbd96422cf08f72e95259b1a0e1d14f
Richard <richardmenning@googlemail.com>
2020-05-02 14:19:09 +0200

    remove local .env

---
+++ pages/goal/success.js
@@ -0,0 +1,68 @@
+import Layout from '../../components/Layout';
+import Card from '../../components/goal/card';
+import Numbers from '../../components/landingpage/numbers';
+
+const success = () => {
+  return (
+    <Layout>
+      <Card>
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-md-3 d-flex justify-content-center">
+              <svg
+                width="144"
+                height="144"
+                viewBox="0 0 144 144"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M72 144C111.765 144 144 111.765 144 72C144 32.2355 111.765 0 72 0C32.2355 0 0 32.2355 0 72C0 111.765 32.2355 144 72 144Z"
+                  fill="#2EBE7C"
+                />
+                <path
+                  d="M62.8009 111.53C60.4529 111.53 58.2324 110.431 56.8049 108.548L37.5251 83.0863C35.0175 79.7748 35.6704 75.0595 38.9819 72.5508C42.2987 70.0411 47.0106 70.6981 49.5194 74.0075L62.1999 90.7515L94.0921 39.544C96.2878 36.0211 100.924 34.9432 104.455 37.1356C107.981 39.3303 109.058 43.9701 106.861 47.4963L69.1865 107.985C67.8775 110.088 65.6182 111.411 63.144 111.523C63.0285 111.528 62.9153 111.53 62.8009 111.53Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+            <div className="col-md-9">
+              <h1>Well Done!</h1>
+              <h3>You have planned your upcoming week!</h3>
+            </div>
+          </div>
+        </div>
+        <div className="container my-5 mx-5">
+          <div className="row">
+            <div className="col">
+              <p>
+                Now you just need to export your task to your favorite task
+                management tool!
+              </p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              <button>
+                Export to <img src="/todoist.png" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="container my-5"></div>
+        <Numbers
+          title="How it works?"
+          stepOneTitle="Set your goals and break them down"
+          stepOneDescription="Excepteur consequat ea reprehenderit nulla excepteur nulla velit."
+          stepTwoTitle="Set your goals and break them down"
+          stepTwoDescription="Excepteur consequat ea reprehenderit nulla excepteur nulla velit."
+          stepThreeTitle="Set your goals and break them down"
+          stepThreeDescription="Excepteur consequat ea reprehenderit nulla excepteur nulla velit."
+        />
+      </Card>
+    </Layout>
+  );
+};
+
+export default success;
