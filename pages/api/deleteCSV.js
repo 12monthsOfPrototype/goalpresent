0467a942a64de5236cacad8f0b82b9ff658aedf9
Richard <richardmenning@googlemail.com>
2020-05-03 18:36:06 +0200

    change folder for csv

--- pages/api/deleteCSV.js
+++ pages/api/deleteCSV.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 export default (req, res) => {
-  fs.unlink('public/tmp/todos.csv', (err) => {
+  fs.unlink('tmp/todos.csv', (err) => {
     if (err) throw err;
 
     res.end();
